4f623cc03473bcb23cbdbb8c7cbb4e7c9fbde915
SevRyna <palna171166@gmail.com>
2023-04-22 17:11:16 +0300

    нові зміни в сторінку index

--- src/route/index.js
+++ src/route/index.js
@@ -37,11 +37,8 @@ var footer = {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-
-
 router.get("/", function (req, res) {
 	// res.render генерує нам HTML сторінку
-
 	//            ↙ cюди вводимо назву файлу з сontainer
 	res.render("index", { layout: "index" });
 	//                  ↑↑ сюди вводимо JSON дані
